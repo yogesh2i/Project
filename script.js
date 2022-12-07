989b706bbdc426481113a908084a1c48717b61e6
yogesh <yogirao8440@gmail.com>
2022-12-07 19:21:41 +0530

    few changes added

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let activity_nav = document.getElementById('activity-nav')
 let activity_menu = document.getElementById('activity-menu')
 let text = document.getElementsByClassName('card-text')
 let log = document.getElementById('log')
+let input = document.getElementById('email-input')
+let pswd = document.getElementById('pswd')
 activity_nav.addEventListener('mouseenter',()=>{
-   
-    
    
     activity_nav.classList.add("show")
     activity_menu.classList.add("show")
@@ -36,7 +36,28 @@ activity_menu.addEventListener('mouseleave',()=>{
    
 })
 log.addEventListener('click',()=>{
-    alert('LogIn successful')
+    let str = input.value
+    let password = pswd.value
+    let reg = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    let test = reg.test(str)
+    let idvalid = false
+    let passvalid = false
+    if(!test){
+        document.getElementById('valid').style.display = 'block';
+    }
+    else{
+        document.getElementById('valid').style.display = 'none';
+        idvalid = true
+    }
+    if(password.length < 3){
+        document.getElementById('pass-valid').style.display = 'block';
+    }else{
+        document.getElementById('pass-valid').style.display = 'none';
+        passvalid = true
+    }
+    if(idvalid && passvalid){
+        alert('login successful')
+    }
 })
 
 
